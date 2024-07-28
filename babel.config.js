354985f9a397f3b959274cbba5827709b18dd0d7
uzairwaheed-8 <uwaheed.ce43ceme@student.nust.edu.pk>
2024-07-28 16:26:31 +0500

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: ["react-app"],
+    plugins: [
+      "@babel/plugin-proposal-private-property-in-object",
+      "transform-react-remove-prop-types"
+    ]
+  };
+  
